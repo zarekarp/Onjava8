240bfb606a61c13408abd15e22bbe52e25368cad
pramod.zarekar <pramod.zarekar@gurucul.com>
2023-03-17 15:03:21 +0530

    fix main class not fount issue

---
+++ src/main/java/org/example/Onjava8/Hello.java
@@ -0,0 +1,7 @@
+package org.example.Onjava8;public class Hello {
+
+    public String doSomething(){
+        String name = "Pramod";
+        return name;
+    }
+}
---
+++ src/main/java/org/example/Onjava8/Onjava8Application.java
@@ -0,0 +1,13 @@
+package org.example.Onjava8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Onjava8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Onjava8Application.class, args);
+	}
+
+}
